37e381a56baa598a747f0a85d7063af112c23a8a
elye <elye.project@gmail.com>
2018-07-16 17:31:37 +1000

    Remove unrelated test

--- app/src/test/java/com/elyeproj/animatedecorator/ExampleUnitTest.kt
+++ app/src/test/java/com/elyeproj/animatedecorator/ExampleUnitTest.kt
@@ -14,29 +14,4 @@ class ExampleUnitTest {
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
     }
-
-    val sequence = generateSequence(1) { it + 1 }.take(50000000)
-    val list = sequence.toList()
-
-    @Test
-    fun testingSomething() {
-
-        println("Chaining Map Filter Sum\t\t " + measureNanoTime {
-            (0 until 50000000).map { list[it] * 2 }.filter { it % 3 == 0 }.map { it * 3 }
-        })
-        println("Sub Normal Map Filter Sum\t " + measureNanoTime {
-            (0 until 50000000).forEach {
-                if ((list[it] * 2) % 3 == 0) {
-                    list[it] * 3
-                }
-            }
-        })
-        println("Normal Map Filter Sum\t " + measureNanoTime {
-            for (i in 0 until 50000000) {
-                if ((list[i] * 2) % 3 == 0) {
-                    list[i] * 3
-                }
-            }
-        })
-    }
 }
